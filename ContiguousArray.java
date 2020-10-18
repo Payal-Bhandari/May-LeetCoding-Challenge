b5f569bd1e184cdb2b7280bf403e91bc0059bb0c
Payal Bhandari <51235238+Payal-Bhandari@users.noreply.github.com>
2020-10-18 20:03:37 +0530

    Create ContiguousArray.java

---
+++ ContiguousArray.java
@@ -0,0 +1,32 @@
+//Contiguous Array
+
+//PROBLEM:
+/*
+Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+*/
+//SOLUTION
+class Solution {
+    public int findMaxLength(int[] nums) {
+        Map<Integer,Integer>map = new HashMap<>();
+        map.put(0,-1);
+        int currSum = 0, ans = 0;
+        for(int i=0;i<nums.length;i++)
+        {
+            currSum += (nums[i]*2-1); 
+            if(map.containsKey(currSum))
+                ans = Math.max(ans, i-map.get(currSum));
+            else map.put(currSum, i);
+        }
+        return ans;
+    }
+}
